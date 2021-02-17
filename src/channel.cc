32e9294cf016bdc2d0381fc4f0e84cfdcb849f54
Pavel Shramov <psha@nguni.psha.org.ru>
2021-02-17 22:16:54 +0300

    sqlite: Initial commit of write-only SQLite channel for TLL

---
+++ src/channel.cc
@@ -0,0 +1,383 @@
+/*
+ * Copyright (c) 2019-2021 Pavel Shramov <shramov@mexmat.net>
+ *
+ * tll is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+#include <sqlite3.h>
+
+#include <variant>
+
+#include "tll/channel/base.h"
+#include "tll/scheme/util.h"
+#include "tll/util/listiter.h"
+#include "tll/util/memoryview.h"
+#include "tll/util/string.h"
+
+using namespace tll;
+
+struct sqliteptr
+{
+	sqlite3 * _db = nullptr;
+	sqliteptr(sqlite3 * db) : _db(db) {}
+	~sqliteptr() { if (_db) sqlite3_close(_db); }
+
+	sqlite3 * operator -> () { return _db; }
+	const sqlite3 * operator -> () const { return _db; }
+
+	operator sqlite3 * () { return _db; }
+	operator const sqlite3 * () const { return _db; }
+};
+
+using query_ptr_t = std::unique_ptr<sqlite3_stmt, decltype(&sqlite3_finalize)>;
+
+class SQLite : public tll::channel::Base<SQLite>
+{
+	std::shared_ptr<sqliteptr> _db;
+	std::map<int, std::pair<const tll::scheme::Message *, query_ptr_t>> _messages;
+
+	std::string _path;
+
+	bool _replace = false;
+	enum class Index { No, Yes, Unique } _seq_index = Index::Unique;
+
+	size_t _bulk_size = 0;
+	size_t _bulk_counter = 0;
+
+ public:
+	static constexpr std::string_view param_prefix() { return "sqlite"; }
+
+	int _init(const tll::Channel::Url &, tll::Channel *master);
+	int _open(const tll::PropsView &);
+	int _close();
+	void _destroy();
+
+	int _post(const tll_msg_t *msg, int flags);
+
+ private:
+	int _create_table(const tll::scheme::Message *);
+	int _create_index(const std::string_view &name, std::string_view key, bool unique);
+
+	sqlite3_stmt * _prepare(const std::string_view query)
+	{
+		_log.debug("Prepare SQL statement:\n\t{}", query);
+		sqlite3_stmt * sql = nullptr;
+		const char * tail = nullptr;
+		auto r = sqlite3_prepare_v2(*_db, query.data(), query.size(), &sql, &tail);
+		if (r != SQLITE_OK)
+			return _log.fail(nullptr, "Failed to prepare statement: {}\n\t{}", sqlite3_errmsg(*_db), query);
+		return sql;
+	}
+};
+
+int SQLite::_init(const Channel::Url &url, Channel * master)
+{
+	if ((internal.caps & (caps::Input | caps::Output)) == caps::Input)
+		return _log.fail(EINVAL, "SQLite channel is write-only");
+
+	if (!_scheme_url)
+		return _log.fail(EINVAL, "Channel needs scheme");
+
+	if (!url.host().size())
+		return _log.fail(EINVAL, "No path to database");
+	_path = url.host();
+
+	auto reader = channel_props_reader(url);
+
+	_replace = reader.getT("replace", false);
+	_seq_index = reader.getT("seq-index", Index::Unique, {{"no", Index::No}, {"yes", Index::Yes}, {"unique", Index::Unique}});
+	_bulk_size = reader.getT("bulk-size", 0u);
+	if (!reader)
+		return _log.fail(EINVAL, "Invalid url: {}", reader.error());
+
+	return 0;
+}
+
+int SQLite::_open(const PropsView &s)
+{
+	_bulk_counter = 0;
+
+	sqlite3 * db = nullptr;
+	int flags = SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE;
+	auto r = sqlite3_open_v2(_path.c_str(), &db, flags, nullptr);
+	if (r)
+		return _log.fail(EINVAL, "Failed to open '{}': {}", _path, sqlite3_errstr(r));
+	_db.reset(new sqliteptr(db));
+
+	if (sqlite3_exec(*_db, "PRAGMA journal_mode=wal", 0, 0, 0))
+		return _log.fail(EINVAL, "Failed to change journal_mode to WAL: {}", sqlite3_errmsg(*_db));
+
+	for (auto & m : tll::util::list_wrap(_scheme->messages)) {
+		if (_create_table(&m))
+			return _log.fail(EINVAL, "Failed to create table for '{}'", m.name);
+	}
+
+	return 0;
+}
+
+namespace {
+
+template <typename Iter>
+std::string join(std::string_view sep, const Iter &begin, const Iter &end)
+{
+	std::string r;
+	bool comma = false;
+	for (Iter it = begin; it != end; ++it) {
+		if (comma)
+			r += sep;
+		comma = true;
+		r += tll::conv::to_string(*it);
+	}
+	return r;
+}
+
+template <typename Iter>
+std::string join(const Iter &begin, const Iter &end)
+{
+	return join(", ", begin, end);
+}
+
+tll::result_t<std::string> sql_type(const tll::scheme::Field *field)
+{
+	using tll::scheme::Field;
+	switch (field->type) {
+	case Field::Int8:
+	case Field::UInt8:
+	case Field::Int16:
+	case Field::UInt16:
+	case Field::Int32:
+	case Field::UInt32:
+	case Field::Int64:
+		return "INTEGER";
+
+	case Field::Double:
+		return "REAL";
+
+	case Field::Decimal128:
+		return tll::error("Decimal128 not supported yet");
+		//return "REAL";
+
+	case Field::Bytes:
+		if (field->sub_type == Field::ByteString)
+			return "VARCHAR";
+		return "REAL";
+
+	case Field::Message:
+		return tll::error("Nested messages not supported");
+	case Field::Array:
+		return tll::error("Nested arrays not supported");
+	case Field::Pointer:
+		if (field->type_ptr->type == Field::Int8 && field->sub_type == Field::ByteString) {
+			return "VARCHAR";
+		}
+		return tll::error("Nested arrays not supported");
+	}
+	return tll::error("Invalid field type");
+}
+
+template <typename Buf>
+int sql_bind(sqlite3_stmt * sql, int idx, const tll::scheme::Field *field, const Buf &data)
+{
+	using tll::scheme::Field;
+	switch (field->type) {
+	case Field::Int8:
+	case Field::Int16:
+	case Field::Int32:
+	case Field::Int64:
+	case Field::UInt8:
+	case Field::UInt16:
+	case Field::UInt32:
+		return sqlite3_bind_int64(sql, idx, tll::scheme::read_size(field, data));
+
+	case Field::Double:
+		return sqlite3_bind_double(sql, idx, *data.template dataT<double>());
+
+	case Field::Decimal128:
+		return SQLITE_ERROR;
+
+	case Field::Bytes:
+		if (field->sub_type == Field::ByteString) {
+			auto str = data.template dataT<char>();
+			return sqlite3_bind_text(sql, idx, str, strnlen(str, field->size), SQLITE_STATIC);
+		}
+
+		return sqlite3_bind_blob(sql, idx, data.data(), field->size, SQLITE_STATIC);
+
+	case Field::Message:
+		return SQLITE_ERROR;
+	case Field::Array:
+		return SQLITE_ERROR;
+	case Field::Pointer:
+		if (field->type_ptr->type == Field::Int8 && field->sub_type == Field::ByteString) {
+			auto ptr = tll::scheme::read_pointer(field, data);
+			if (!ptr)
+				return SQLITE_ERROR;
+			if (ptr->size == 0)
+				return sqlite3_bind_text(sql, idx, "", 0, SQLITE_STATIC);
+			return sqlite3_bind_text(sql, idx, data.view(ptr->offset).template dataT<char>(), ptr->size - 1, SQLITE_STATIC);
+		}
+		return SQLITE_ERROR;
+	}
+	return SQLITE_ERROR;
+}
+
+}
+
+int SQLite::_create_table(const tll::scheme::Message * msg)
+{
+	std::string_view table(msg->name);
+	if (msg->msgid == 0) {
+		_log.debug("Message {} has no msgid, skip table check", table);
+		return 0;
+	}
+
+	query_ptr_t sql = { nullptr, sqlite3_finalize };
+
+	sql.reset(_prepare("SELECT name FROM sqlite_master WHERE name=?"));
+	if (!sql)
+		return _log.fail(EINVAL, "Failed to prepare check statement");
+
+	sqlite3_bind_text(sql.get(), 1, table.data(), table.size(), SQLITE_STATIC);
+	auto r = sqlite3_step(sql.get());
+	if (r == SQLITE_ROW) {
+		_log.debug("Table '{}' exists", table);
+		return 0;
+	} else if (r != SQLITE_DONE)
+		return _log.fail(EINVAL, "Failed to check table '{}'", table);
+
+	_log.info("Create table '{}'", table);
+	std::list<std::string> fields;
+
+	fields.push_back("`_tll_seq` INTEGER");
+	for (auto & f : tll::util::list_wrap(msg->fields)) {
+		auto t = sql_type(&f);
+		if (!t)
+			return _log.fail(EINVAL, "Message {} field {}: {}", msg->name, f.name, t.error());
+		fields.push_back(fmt::format("`{}` {} NOT NULL", f.name, *t));
+
+		auto pkey = tll::getter::getT(f.options, "primary-key", false);
+		if (f.type == f.Pointer)
+			pkey = tll::getter::getT(f.type_ptr->options, "primary-key", false);
+
+		if (!pkey)
+			_log.warning("Invalid primary-key option: {}", pkey.error());
+		else if (*pkey) {
+			_log.debug("Field {} is primary key", f.name);
+			fields.back() += " PRIMARY KEY";
+		}
+	}
+
+	sql.reset(_prepare(fmt::format("CREATE TABLE `{}` ({})", table, join(fields.begin(), fields.end()))));
+	if (!sql)
+		return _log.fail(EINVAL, "Failed to prepare CREATE statement");
+
+	if (sqlite3_step(sql.get()) != SQLITE_DONE)
+		return _log.fail(EINVAL, "Failed to create table '{}'", table);
+
+	if (_seq_index != Index::No) {
+		if (_create_index(table, "_tll_seq", _seq_index == Index::Unique))
+			return _log.fail(EINVAL, "Failed to create seq index for table {}", table);
+	}
+	auto key = tll::getter::get(msg->options, "key");
+	if (key) {
+		if (_create_index(table, *key, false))
+			return _log.fail(EINVAL, "Failed to create index {} for table {}", *key, table);
+	}
+
+	key = tll::getter::get(msg->options, "unique-key");
+	if (key) {
+		if (_create_index(table, *key, true))
+			return _log.fail(EINVAL, "Failed to create unique index {} for table {}", *key, table);
+	}
+
+	std::list<std::string> names;
+	names.push_back("`_tll_seq`");
+	for (auto & f : tll::util::list_wrap(msg->fields))
+		names.push_back(fmt::format("`{}`", f.name));
+
+	std::string_view operation = "INSERT";
+	if (_replace)
+		operation = "REPLACE";
+	auto insert = fmt::format("{} INTO `{}`({}) VALUES ", operation, table, join(names.begin(), names.end()));
+	for (auto & i : names)
+		i = "?";
+	insert += fmt::format("({})", join(names.begin(), names.end()));
+
+	sql.reset(_prepare(insert));
+	if (!sql)
+		return _log.fail(EINVAL, "Failed to prepare update statement for table {}: {}", table, insert);
+
+	_messages.emplace(msg->msgid, std::make_pair(msg, std::move(sql)));
+
+	return 0;
+}
+
+int SQLite::_create_index(const std::string_view &name, std::string_view key, bool unique)
+{
+	_log.debug("Create index for {}: key {}", name, key);
+	query_ptr_t sql = { nullptr, sqlite3_finalize };
+
+	std::string_view ustr = unique ? "UNIQUE " : "";
+	auto str = fmt::format("CREATE {} INDEX `_tll_{}_{}` on `{}`(`{}`)", ustr, name, key, name, key);
+	sql.reset(_prepare(str));
+	if (!sql)
+		return _log.fail(EINVAL, "Failed to prepare index statement: {}", str);
+
+	if (sqlite3_step(sql.get()) != SQLITE_DONE)
+		return _log.fail(EINVAL, "Failed to create index for '{}': {}", name, key);
+	return 0;
+}
+
+int SQLite::_close()
+{
+	if (_bulk_counter) {
+		if (sqlite3_exec(*_db, "COMMIT", 0, 0, 0))
+			_log.error("Failed to commit pending transaction: {}", sqlite3_errmsg(*_db));
+		_bulk_counter = 0;
+	}
+	_messages.clear();
+	_db.reset();
+	return 0;
+}
+
+int SQLite::_post(const tll_msg_t *msg, int flags)
+{
+	if (msg->type != TLL_MESSAGE_DATA)
+		return 0;
+	if (msg->msgid == 0)
+		return _log.fail(EINVAL, "Unable to insert message without msgid");
+	auto it = _messages.find(msg->msgid);
+	if (it == _messages.end())
+		return _log.fail(ENOENT, "Message {} not found", msg->msgid);
+	auto & [message, insert] = it->second;
+
+	if (!_bulk_counter) {
+		sqlite3_exec(*_db, "BEGIN", 0, 0, 0);
+	}
+
+	sqlite3_reset(insert.get());
+
+	auto view = tll::make_view(*msg);
+
+	int idx = 1;
+	sqlite3_bind_int64(insert.get(), idx++, (sqlite3_int64) msg->seq);
+	for (auto & f : tll::util::list_wrap(message->fields)) {
+		if (sql_bind(insert.get(), idx++, &f, view.view(f.offset)))
+			return _log.fail(EINVAL, "Failed to bind field {}", f.name);
+	}
+	auto r = sqlite3_step(insert.get());
+	if (r != SQLITE_DONE)
+		return _log.fail(EINVAL, "Failed to insert data: {}", sqlite3_errmsg(*_db));
+	if (++_bulk_counter >= _bulk_size) {
+		_log.debug("Commit");
+		if (sqlite3_exec(*_db, "COMMIT", 0, 0, 0))
+			_log.error("Commit failed: {}", sqlite3_errmsg(*_db));
+		_bulk_counter = 0;
+	}
+	return 0;
+}
+
+TLL_DEFINE_IMPL(SQLite);
+
+auto channel_module = tll::make_channel_module<SQLite>();
---
+++ test.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+# vim: sts=4 sw=4 et
+
+import os
+import sqlite3
+
+import pytest
+
+from tll.channel import Context
+from tll.error import TLLError
+
+SCHEME = '''yamls://
+- name: ignored
+  fields:
+    - {name: s0, type: '**int8'}
+- name: scalar
+  id: 10
+  options.key: i8
+  fields:
+    - {name: i8, type: int8}
+    - {name: i16, type: int16}
+    - {name: i32, type: int32}
+    - {name: i64, type: int64}
+    - {name: u8, type: uint8}
+    - {name: u16, type: uint16}
+    - {name: u32, type: uint32}
+    - {name: d, type: double}
+- name: text
+  id: 20
+  fields:
+    - {name: b, type: byte8}
+    - {name: f, type: byte32, options.type: string}
+    - {name: s, type: string, options.primary-key: true}
+'''
+
+@pytest.fixture
+def context():
+    ctx = Context()
+    ctx.load(os.path.join(os.environ.get("BUILD_DIR", "build"), "tll-sqlite"), 'channel_module')
+    return ctx
+
+@pytest.fixture
+def db_file(tmp_path):
+    return tmp_path / 'test.db'
+
+def test_insert_unique(context, db_file):
+    db = sqlite3.connect(db_file)
+    c = context.Channel(f'sqlite://{db_file};replace=false;seq-index=unique', scheme=SCHEME, dump='scheme')
+    c.open()
+
+    c.post(name='scalar', data={'i8':-8, 'i16':-16, 'i32':-32, 'i64':-64, 'u8':8, 'u16':16, 'u32':32, 'd':1.23}, seq=1)
+    with pytest.raises(TLLError): c.post(name='scalar', data={}, seq=1)
+
+    assert list(db.cursor().execute('SELECT * FROM `scalar`')) == [(1, -8, -16, -32, -64, 8, 16, 32, 1.23)]
+
+    c.post(name='text', data={'b':b'bytes', 'f':'fixed string', 's':'offset string'}, seq=1)
+
+    assert list(db.cursor().execute('SELECT * FROM `text`')) == [(1, b'bytes\0\0\0', 'fixed string', 'offset string')]
+
+def test_replace(context, db_file):
+    db = sqlite3.connect(db_file)
+    c = context.Channel(f'sqlite://{db_file};replace=true;seq-index=unique', scheme=SCHEME, dump='scheme')
+    c.open()
+
+    c.post(name='scalar', data={'i8':-8, 'i16':-16, 'i32':-32, 'i64':-64, 'u8':8, 'u16':16, 'u32':32, 'd':1.23}, seq=1)
+    assert list(db.cursor().execute('SELECT * FROM `scalar`')) == [(1, -8, -16, -32, -64, 8, 16, 32, 1.23)]
+
+    c.post(name='scalar', data={'i8': 100}, seq=1)
+
+    assert list(db.cursor().execute('SELECT * FROM `scalar`')) == [(1, 100, 0, 0, 0, 0, 0, 0, 0)]
+
+    c.post(name='text', data={'b':b'bytes', 'f':'fixed string', 's':'key'}, seq=1)
+
+    assert list(db.cursor().execute('SELECT * FROM `text`')) == [(1, b'bytes\0\0\0', 'fixed string', 'key')]
+
+    c.post(name='text', data={'b':b'other', 'f':'other string', 's':'key'}, seq=2)
+
+    assert list(db.cursor().execute('SELECT * FROM `text`')) == [(2, b'other\0\0\0', 'other string', 'key')]
+
+def test_insert(context, db_file):
+    db = sqlite3.connect(db_file)
+    c = context.Channel(f'sqlite://{db_file};replace=false;seq-index=no', scheme=SCHEME, dump='scheme')
+    c.open()
+
+    c.post(name='scalar', data={'i8':-8, 'i16':-16, 'i32':-32, 'i64':-64, 'u8':8, 'u16':16, 'u32':32, 'd':1.23})
+    assert list(db.cursor().execute('SELECT * FROM `scalar`')) == [(0, -8, -16, -32, -64, 8, 16, 32, 1.23)]
+
+    c.post(name='scalar', data={'i8':-9, 'i16':-17, 'i32':-33, 'i64':-65, 'u8':9, 'u16':17, 'u32':33, 'd':2.34})
+
+    assert list(db.cursor().execute('SELECT * FROM `scalar`')) == [
+        (0, -8, -16, -32, -64, 8, 16, 32, 1.23),
+        (0, -9, -17, -33, -65, 9, 17, 33, 2.34),
+    ]
+
+    c.post(name='text', data={'b':b'bytes', 'f':'fixed string', 's':'key'})
+
+    assert list(db.cursor().execute('SELECT * FROM `text`')) == [(0, b'bytes\0\0\0', 'fixed string', 'key')]
+
+    with pytest.raises(TLLError): c.post(name='text', data={'b':b'other', 'f':'other string', 's':'key'})
+    c.post(name='text', data={'b':b'other', 'f':'other string', 's':'key-1'})
+
+    assert list(db.cursor().execute('SELECT * FROM `text`')) == [
+        (0, b'bytes\0\0\0', 'fixed string', 'key'),
+        (0, b'other\0\0\0', 'other string', 'key-1'),
+    ]
+
+BULK = '''yamls://
+- name: msg 
+  id: 10
+  fields:
+    - {name: field, type: int32}
+'''
+
+def test_bulk(context, db_file):
+    db = sqlite3.connect(db_file)
+    c = context.Channel(f'sqlite://{db_file};replace=false;bulk-size=10', scheme=BULK, dump='scheme')
+    c.open()
+
+    for i in range(5):
+        c.post(name='msg', data={'field': i}, seq=i)
+
+    assert list(db.cursor().execute('SELECT * FROM `msg`')) == []
+
+    for i in range(5, 10):
+        c.post(name='msg', data={'field': i}, seq=i)
+
+    assert list(db.cursor().execute('SELECT * FROM `msg`')) == [(i, i) for i in range(10)]
+
+    for i in range(10, 15):
+        c.post(name='msg', data={'field': i}, seq=i)
+
+    assert list(db.cursor().execute('SELECT * FROM `msg`')) == [(i, i) for i in range(10)]
+
+    c.close()
+
+    assert list(db.cursor().execute('SELECT * FROM `msg`')) == [(i, i) for i in range(15)]
