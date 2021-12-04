9d5491e2c7047c926d629ed3fb903b35ec61aa52
Pavel Shramov <psha@nguni.psha.org.ru>
2021-12-04 19:11:35 +0300

    sqlite: Disable clang formatting for oneline structures

--- src/channel.cc
+++ src/channel.cc
@@ -17,8 +17,10 @@
 
 using namespace tll;
 
+// clang-format off
 struct sqlite3_delete { void operator ()(sqlite3 *ptr) const { sqlite3_close(ptr); } };
 struct sqlite3_stmt_delete { void operator ()(sqlite3_stmt *ptr) const { sqlite3_finalize(ptr); } };
+// clang-format on
 
 using query_ptr_t = std::unique_ptr<sqlite3_stmt, sqlite3_stmt_delete>;
 
